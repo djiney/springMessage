993e838b0e41032dfa54d52532fa21bd16c07cc4
Djiney <DjineyX@gmail.com>
2020-04-12 15:05:32 +0300

    Initial commit

---
+++ src/main/java/telran/spring/message/MessageSenderApplication.java
@@ -0,0 +1,15 @@
+package telran.spring.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageSenderApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(MessageSenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/telran/spring/message/MessageSenderApplicationTests.java
@@ -0,0 +1,15 @@
+package telran.spring.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageSenderApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
